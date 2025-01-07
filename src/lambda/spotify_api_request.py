9c0a72fc91b4139a07f6c01835106c7b3d0e8229
Jiwan Heo <jiwanheo123@gmail.com>
2025-01-07 11:51:23 -0500

    d

--- src/lambda/spotify_api_request.py
+++ src/lambda/spotify_api_request.py
@@ -122,8 +122,8 @@ def lambda_handler(event, context):
     #         }
     #     }
     ssm_client = boto3.client('ssm')
-    # logger.info("hihi")
-    # print("helo")
+    logger.info("hihi")
+    print("helo")
     try:
         # Fetch the Spotify token from Parameter Store
         token = ssm_client.get_parameter(
